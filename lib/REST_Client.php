7806efca847a606a5bfd559d481c0d4e086911cc
Xeloses <37206095+Xeloses@users.noreply.github.com>
2021-04-13 14:18:12 +0300

    fix spelling

--- lib/REST_Client.php
+++ lib/REST_Client.php
@@ -7,7 +7,7 @@
  * @package    RESTy (https://github.com/Xeloses/resty)
  * @version    1.0.1
  * @copyright  Xeloses 2019-2020
- * @license    MIT (http://en.wikipedia.org/wiki/MIT_License)
+ * @license    GNU GPL v3 (https://www.gnu.org/licenses/gpl-3.0.html)
  */
 
 namespace Xeloses\RESTy;
